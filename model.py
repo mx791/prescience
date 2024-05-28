2c5b9d440a4598f865d73f6fbf418de72d0529bc
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-05-28 16:46:09 +0200

    Update model.py

--- model.py
+++ model.py
@@ -41,7 +41,7 @@ def recursive_regressor(dataframe: pd.DataFrame, date_col: str, target_col: str,
               best_score = score
               best_model = modl
             
-    open("report.html", "w+").write(best_model.report())
+    open("./out/report.html", "w+").write(best_model.report())
     return best_model, best_score
 
 
