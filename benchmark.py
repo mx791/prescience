28ca7b000a1be45c30f052923f7bc19f392f64dd
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-05-28 16:07:48 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -16,6 +16,7 @@ def test(name):
     p = reg.predict(data)
     print("r2", r2_score(data["y"].values, p))
     print(reg.describe())
+    reg.report()
 
 test("1J7")
 test("8QR")
@@ -34,6 +35,7 @@ def weather_test():
     p = reg.predict(data)
     print("r2", r2_score(data["y"].values, p))
     print(reg.describe())
+    reg.report()
 
 
 weather_test()
